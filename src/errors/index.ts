527a3ea8f809773a51753a1a80b09b83d864a6d2
QED0711 <qdizon@gmail.com>
2023-02-28 05:52:06 -0500

    added reserved state key error

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -12,6 +12,13 @@ export class ProtectedNamespaceError extends Error {
     }
 }
 
+export class ReservedStateKeyError extends Error {
+    constructor(message: string){
+        super(message);
+        this.name = "ReservedStateKeyError";
+    }
+}
+
 export class ManagerNotFoundError extends Error {
     constructor(message: string){
         super(message);
